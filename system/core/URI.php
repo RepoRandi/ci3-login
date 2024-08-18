c6418566379992bf1d9d33c1b09d75e041ec7361
Randi <devs.randi@gmail.com>
2024-08-18 22:49:00 +0700

    initial commit

--- system/core/URI.php
+++ system/core/URI.php
@@ -50,7 +50,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
  * @author		EllisLab Dev Team
  * @link		https://codeigniter.com/userguide3/libraries/uri.html
  */
-
+#[\AllowDynamicProperties]
 class CI_URI
 {
 
